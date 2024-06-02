0db086008c8e9219f95af326d551d9a1258f429d
ktr17 <9158853+ktr17@users.noreply.github.com>
2024-06-02 14:07:11 +0900

    console画面に文字起こしを出力するように修正（promptでは文字数制限があるため。）

--- GetYouTubeScript.js
+++ GetYouTubeScript.js
@@ -1,17 +1,19 @@
 javascript:(
-    function(){
-      scriptList = document.querySelector("#segments-container").children;
-      outputText = "";
-  
-      for (var i = 0; i < scriptList.length; i++){
-        script = scriptList[i].children[0].getElementsByClassName("segment-text style-scope ytd-transcript-segment-renderer")[0];
-        if (script != undefined)
-        {
-          outputText += script.innerText + " ";
-        }
-      }
-      prompt("Please copy text.", `${outputText}\n`);
-    }
-  )();
+  function(){
+    scriptList = document.querySelector("#segments-container").children;
+    outputText = "";
 
-  document.querySelector("#segments-container").children[0].getElementsByClassName("segment-text style-scope ytd-transcript-segment-renderer")
+    for (var i = 0; i < scriptList.length; i++){
+      script = scriptList[i].children[0].getElementsByClassName("segment-text style-scope ytd-transcript-segment-renderer")[0];
+      if (script != undefined)
+      {
+        outputText += script.innerText + " ";
+        
+      }
+      else{
+        console.log("error");
+      }
+    }
+    console.log(outputText);
+  }
+)();
