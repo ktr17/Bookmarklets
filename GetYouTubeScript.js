dc58e003bf794655172cfe61a207bc40ebc90706
ktr17 <9158853+ktr17@users.noreply.github.com>
2024-06-02 12:16:22 +0900

    html要素を取得できない場合のエラー処理を追加

--- GetYouTubeScript.js
+++ GetYouTubeScript.js
@@ -5,8 +5,13 @@ javascript:(
   
       for (var i = 0; i < scriptList.length; i++){
         script = scriptList[i].children[0].getElementsByClassName("segment-text style-scope ytd-transcript-segment-renderer")[0];
-        outputText += script.innerText + " ";
+        if (script != undefined)
+        {
+          outputText += script.innerText + " ";
+        }
       }
       prompt("Please copy text.", `${outputText}\n`);
     }
   )();
+
+  document.querySelector("#segments-container").children[0].getElementsByClassName("segment-text style-scope ytd-transcript-segment-renderer")
